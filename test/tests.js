8181bac40f769271aa8585287718ce5f074c0b8a
Jörn Röder <kontakt@joernroeder.de>
2016-12-15 14:08:34 +0100

    added test scripts, dummy test and travis.yml

---
+++ test/tests.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var piwikReactRouter = require('../');
+
+describe('piwikReactRouter', () => {
+
+  it ('should work', () => {
+    assert.equal(2+2, 4);
+  });
+});
