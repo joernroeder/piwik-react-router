2479473b2cf3ed51d81117a3d6ebd18e464ef370
Jörn Röder <kontakt@joernroeder.de>
2016-12-16 11:10:18 +0100

    added shim tests for serverside rendering

--- test/server.tests.js
+++ test/server.tests.js
@@ -19,4 +19,27 @@ describe('piwik react router serverside tests', function () {
     assert.isTrue(piwikReactRouter._isShim);
   });
 
+  it ('should correctly return a noop function for every public api method', () => {
+    const piwikReactRouter = testUtils.requireNoCache('../')(({
+      siteId: 1,
+      url: 'foo.bar'
+    }));
+
+    testUtils.API_METHOD_NAMES.forEach(methodName => {
+      assert.isFunction(piwikReactRouter[methodName]);
+      assert.isUndefined(piwikReactRouter[methodName]());
+    });
+  });
+
+  it ('should correctly return the passed in history object from the mocked connectToHistory method', () => {
+    const history = { history: true };
+
+    const piwikReactRouter = testUtils.requireNoCache('../')(({
+      siteId: 1,
+      url: 'foo.bar'
+    }));
+
+    assert.strictEqual(history, piwikReactRouter.connectToHistory(history));
+  });
+
 });
