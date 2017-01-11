f209dcc140e0d2265ea6ff08681065e941eeffd4
Jörn Röder <kontakt@joernroeder.de>
2017-01-11 15:10:29 +0100

    added proxyquire for module mocking

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,13 @@
+const proxyquire = require('proxyquire');
 
 // copied from https://gist.github.com/adam-lynch/11037907
 
 const _invalidateRequireCacheForFile = function (filePath) {
 	delete require.cache[require.resolve(filePath)];
 };
-const requireNoCache = function (filePath) {
+const requireNoCache = function (filePath, opts) {
 	_invalidateRequireCacheForFile(filePath);
-	return require(filePath);
+	return opts ? proxyquire(filePath, opts) : require(filePath);
 };
 
 const API_METHOD_NAMES = [
