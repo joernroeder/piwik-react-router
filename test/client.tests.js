4e2ad75cbd12223be76bdbbe5e273b000f29b202
Jörn Röder <kontakt@joernroeder.de>
2017-03-20 10:48:37 +0100

    increased test coverage

--- test/client.tests.js
+++ test/client.tests.js
@@ -339,6 +339,30 @@ describe('piwik-react-router client tests', function () {
         [ 'trackPageView' ]
       ]);
     });
+
+    it ('should correctly ignore the initial visit if the appropriate option is enabled', () => {
+      const piwikReactRouter = testUtils.requireNoCache('../')({
+        url: 'foo.bar',
+        siteId: 1,
+        ignoreInitialVisit: true
+      });
+
+      const unlistenFn = sinon.spy();
+      let listenStub = sinon.stub().returns(unlistenFn);
+
+      const history = {
+        listen: listenStub,
+        location: {
+          pathname: '/foo/bar.html',
+          search: '?foo=bar'
+        }
+      };
+
+      piwikReactRouter.track = sinon.spy();
+      piwikReactRouter.connectToHistory(history);
+
+      assert.isFalse(piwikReactRouter.track.called);
+    });
   });
 
   it ('should correctly handle basename', () => {
